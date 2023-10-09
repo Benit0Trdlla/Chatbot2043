0d10e18c566ea704739122b4383425c87ca0afc2
inaki <inakivalero04@gmail.com>
2023-10-09 10:07:25 -0300

    Arreglado POST a node-red

--- botManagement.js
+++ botManagement.js
@@ -1,45 +1,62 @@
-const chatBody = document.getElementById('chat-body');
-const userInput = document.getElementById('userInput');
-const sendButton = document.getElementById('send');
+const chatBody = document.getElementById("chat-body");
+const userInput = document.getElementById("userInput");
+const sendButton = document.getElementById("send");
 
 function addUserMessage(message) {
-    const userMessage = document.createElement('div');
-    userMessage.classList.add('message', 'user-message');
-    userMessage.textContent = message;
-    chatBody.appendChild(userMessage);
+  const userMessage = document.createElement("div");
+  userMessage.classList.add("bg-secondary", "chat", "chat-bubble", "chat-end");
+  userMessage.textContent = message;
+  chatBody.appendChild(userMessage);
 }
 
 function addBotMessage(message) {
-    const botMessage = document.createElement('div');
-    botMessage.classList.add('message', 'bot-message');
-    botMessage.textContent = message;
-    chatBody.appendChild(botMessage);
+  const botMessage = document.createElement("div");
+  botMessage.classList.add("bg-secondary", "chat", "chat-bubble", "chat-start");
+  botMessage.textContent = message;
+  chatBody.appendChild(botMessage);
 }
 
-const botText1 = "¡Hola! Soy un bot de asistencia. ¿Qué puedo hacer para ayudarte?";
+const botText1 =
+  "¡Hola! Soy un bot de asistencia. ¿Qué puedo hacer para ayudarte?";
 addBotMessage(botText1);
 
 function handleSendMessage() {
-    const userMessage = userInput.value.trim();
+  const userMessage = userInput.value.trim();
 
-    if (userMessage !== '') {
-        addUserMessage(userMessage);
-        console.log(userMessage);
-        userInput.value = ''; 
-    }
-    alert('4kt');
+  if (userMessage !== "") {
+    addUserMessage(userMessage);
+    console.log(userMessage);
+    userInput.value = "";
 
-    //ACA SE TIENE QUE HACER EL FECTH.
+    fetch("http://localhost:1880/sexo", {
+      method: "POST",
+      body: JSON.stringify(userMessage),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const botMessage = data.botMessage;
+
+        addBotMessage(botMessage);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  //   const formData = new FormData();
+  //   formData.append("userInput", message);
+
+  //ACA SE TIENE QUE HACER EL FECTH.
 }
 
-sendButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    handleSendMessage();
+sendButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  handleSendMessage();
 });
 
-userInput.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        handleSendMessage();
-    }
+userInput.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    handleSendMessage();
+  }
 });
