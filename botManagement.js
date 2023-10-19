b205e10ce0ef41b1b35c20fae349da659ec2d88e
Benit0Trdlla <benilorenzo007op@gmail.com>
2023-10-19 17:35:09 -0300

    Estilos chatbot sin tailwind.

--- botManagement.js
+++ botManagement.js
@@ -1,45 +1,65 @@
 const chatBody = document.getElementById('chat-body');
-const userInput = document.getElementById('userInput');
-const sendButton = document.getElementById('send');
+const chatHeader = document.getElementById("chat-header");
+const userInput = document.getElementById('user-input');
 
 function addUserMessage(message) {
-    const userMessage = document.createElement('div');
-    userMessage.classList.add('message', 'user-message');
-    userMessage.textContent = message;
-    chatBody.appendChild(userMessage);
+	const userMessage = document.createElement('div');
+	userMessage.classList.add('message', 'user-message');
+	userMessage.textContent = message;
+	chatBody.appendChild(userMessage);
 }
 
+
 function addBotMessage(message) {
-    const botMessage = document.createElement('div');
-    botMessage.classList.add('message', 'bot-message');
-    botMessage.textContent = message;
-    chatBody.appendChild(botMessage);
+	const botMessage = document.createElement('div');
+	botMessage.classList.add('message', 'bot-message');
+	botMessage.textContent = message;
+	chatBody.appendChild(botMessage);
 }
 
 const botText1 = "¡Hola! Soy un bot de asistencia. ¿Qué puedo hacer para ayudarte?";
 addBotMessage(botText1);
 
-function handleSendMessage() {
-    const userMessage = userInput.value.trim();
 
-    if (userMessage !== '') {
-        addUserMessage(userMessage);
-        console.log(userMessage);
-        userInput.value = ''; 
-    }
-    alert('4kt');
+function processUserInput() {
+	const message = userInput.value.trim();
+	if (message === '') {
+		return; 
+	}
 
-    //ACA SE TIENE QUE HACER EL FECTH.
+	addUserMessage(message);
+	userInput.value = '';
+
+	const formData = new FormData();
+	formData.append('userInput', message);
+
+	fetch("http://192.168.2.149:1880/chat", {
+		method: 'POST',
+		body: formData
+	})
+		.then(response => response.json())
+		.then(data => {
+			const botMessage = data.botMessage;
+			addBotMessage(botMessage);
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
 }
 
-sendButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    handleSendMessage();
+userInput.addEventListener('keydown', (event) => {
+	if (event.keyCode === 13) {
+		event.preventDefault(); 
+		processUserInput();
+		document.getElementById("user-input").value = '';
+	}
 });
 
-userInput.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        handleSendMessage();
-    }
-});
+
+
+
+
+
+
+
+
